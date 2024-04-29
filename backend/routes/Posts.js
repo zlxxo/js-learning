fae77441dc2c798a7e1c14d68ef4a96ee1d0fe6b
Muki <mramic@student.tugraz.at>
2024-04-29 18:31:09 +0200

    create new post and get all posts

--- backend/routes/Posts.js
+++ backend/routes/Posts.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.send("Hello world");
+const {Posts} = require("../models")
+
+router.get("/", async (req, res) => {
+    const allPosts = await Posts.findAll()
+
+    res.json(allPosts)
+});
+
+router.post("/", async (req, res) => {
+    const post = req.body
+
+    await Posts.create(post); // post is an object with same format as table
+
+    res.json(post)
+
 });
 
 module.exports = router;
