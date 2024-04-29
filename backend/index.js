7b2638b4d4ac82b718be45a43cff8fad31eae91d
Muki <mramic@student.tugraz.at>
2024-04-29 18:30:06 +0200

    parse requests as json

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,15 @@
 const express = require('express')
 const app = express()
 
+// parse to json format in requests
+app.use(express.json())
+
 // sequelize datbase
 const db = require('./models')
 
 // routers
 const postRouter = require('./routes/Posts')
-app.use("/post", postRouter) // route for posts is /post == localhost/post
+app.use("/posts", postRouter) // route for posts is /post == localhost/post
 
 
 // create database
