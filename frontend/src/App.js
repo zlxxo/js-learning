dd35c757008c444ddf06dac0bc7fff6cd1624583
Muki <mramic@student.tugraz.at>
2024-04-29 19:19:46 +0200

    show all posts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,31 @@
 import './App.css';
+import axios from 'axios'
+import {useEffect, useState} from 'react'
 
 function App() {
+
+  const [postsList, setPostsList] = useState([])
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/posts").then((response) => {
+      setPostsList(response.data)
+    })
+  }, [])
+
   return (
-    <div className="App">
+    <div className="App"> {
+      postsList.map((value, key) => {
+        return <div class="post">
+          <div class="title">
+            <p clas="user"> {value.user} </p>
+            <p clas="post-title"> {value.title} </p>
+          </div>
+          <div class="text">
+            <p class="post-text"> {value.text} </p>
+          </div>
+        </div>
+      })
+    }
     </div>
   );
 }
