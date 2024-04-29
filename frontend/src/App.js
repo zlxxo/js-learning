81e94e999d819a173ab5a7d79f049fb2ef469e7d
Muki <mramic@student.tugraz.at>
2024-04-29 20:41:10 +0200

    moved front page to different file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,38 +1,9 @@
 import './App.css';
-import axios from 'axios'
-import {useEffect, useState} from 'react'
 
 function App() {
 
-  const [postsList, setPostsList] = useState([])
-
-  useEffect(() => {
-    axios.get("http://localhost:3001/posts").then((response) => {
-      setPostsList(response.data)
-    })
-  }, [])
-
   return (
     <div className="App">
-      <div class="pageHeader">
-        <p class="pageTitle"> Note App</p>
-      </div>
-
-      <div class="content">
-        {
-        postsList.map((value, key) => {
-          return <div class="post">
-            <div class="header">
-              <p clas="user"> @{value.user} </p>
-            </div>
-            <div class="body">
-              <p clas="title"> {value.title} </p>
-              <p class="text"> {value.text} </p>
-            </div>
-          </div>
-        })
-        }
-      </div>
     </div>
   );
 }
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import axios from 'axios'
+import {useEffect, useState} from 'react'
+
+export default function Home() {
+
+    const [postsList, setPostsList] = useState([])
+
+    useEffect(() => {
+        axios.get("http://localhost:3001/posts").then((response) => {
+          setPostsList(response.data)
+        })
+      }, [])
+
+  return (
+    <div>
+      <div class="pageHeader">
+        <p class="pageTitle"> Note App</p>
+      </div>
+
+      <div class="content">
+        {
+        postsList.map((value, key) => {
+          return <div class="post">
+            <div class="header">
+              <p clas="user"> @{value.user} </p>
+            </div>
+            <div class="body">
+              <p clas="title"> {value.title} </p>
+              <p class="text"> {value.text} </p>
+            </div>
+          </div>
+        })
+        }
+      </div>
+    </div>
+  )
+}
