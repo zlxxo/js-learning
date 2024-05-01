e53eb0d63aebf7b27f366166a94794c3395d0a38
Muki <mramic@student.tugraz.at>
2024-05-01 23:46:18 +0200

    display specific post

--- frontend/src/pages/Post.js
+++ frontend/src/pages/Post.js
@@ -1,13 +1,37 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import {useParams} from 'react-router-dom'
+import axios from 'axios';
 
 function Post() {
     let {id} = useParams();
+
+    const [post, setPost] = useState({})
+
+    useEffect(() => {
+        axios.get(`http://localhost:3001/posts/id/${id}`).then((response) => {
+            if(response != null) {
+                setPost(response.data)
+            }
+        })
+    }, [])
+
   return (
     <div>
-        <div class="content">
-            <h1>Post</h1>
-            <p>{id}</p>
+        <div class="contentPost">
+            <div class="left">
+                <div class="postCom">
+                    <div class="header">
+                        <p clas="user"> @{post.user} </p>
+                    </div>
+                    <div class="body">
+                        <p clas="title"> {post.title} </p>
+                        <p class="text"> {post.text} </p>
+                </div>
+            </div>
+            </div>
+            <div class="right">
+                Comments
+            </div>
         </div>
     </div>
   )
