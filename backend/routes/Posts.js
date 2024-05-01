f65edf6fcc544437b221e67182291ad3422f8ed5
Muki <mramic@student.tugraz.at>
2024-05-01 23:45:44 +0200

    route to get post by id

--- backend/routes/Posts.js
+++ backend/routes/Posts.js
@@ -9,6 +9,14 @@ router.get("/", async (req, res) => {
     res.json(allPosts)
 });
 
+router.get("/id/:id", async (req, res) => {
+    const id = req.params.id;
+
+    const post = await Posts.findByPk(id)
+
+    res.json(post)
+});
+
 router.post("/", async (req, res) => {
     const post = req.body
 
