0744d7d38159f9691e66a4f148c2e3cdeaa95906
Muki <mramic@student.tugraz.at>
2024-05-05 18:36:10 +0200

    log in and sign up pages files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import {BrowserRouter as Router, Route, Routes as Switch} from 'react-router-dom
 import Home from './pages/Home'
 import CreatePost from './pages/CreatePost';
 import Post from './pages/Post';
+import SignUp from './pages/SignUp';
+import LogIn from './pages/LogIn';
 
 function App() {
 
@@ -15,6 +17,8 @@ function App() {
     <ul>
       <li><a href="/">Home</a></li>
       <li><a href="/create">Create a Post</a></li>
+      <li><a href="/login">Log In</a></li>
+      <li><a href="/signup">Sign Up</a></li>
     </ul>
   </div>
 </div>
@@ -23,6 +27,8 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/create" element={<CreatePost />} />
           <Route path="/post/:id" element={<Post />} />
+          <Route path="/login" element={<LogIn />} />
+          <Route path="/signup" element={<SignUp />} />
         </Switch>
       </Router>
     </div>
